aec6b85d168351756299179b26255d8da49d9837
ashutosh15025 <79096320+ashutosh15025@users.noreply.github.com>
2024-05-14 13:54:00 +0530

    some commits

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -56,22 +56,54 @@ class _HomeState extends State<Home> {
             MoltenTab(
               icon: Icon(Icons.home,
                 color:  _selectedIndex==0?Colors.white:Colors.blueGrey,),
+              title: Text("Home",
+                style: GoogleFonts.baloo2(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400
+                ),)
             ),
             MoltenTab(
               icon: Icon(Icons.groups,
                 color:  _selectedIndex==1?Colors.white:Colors.blueGrey,),
+                title: Text("Friends",
+                  style: GoogleFonts.baloo2(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400
+                  ),)
+
             ),
             MoltenTab(
               icon: Icon(Icons.chat,
                 color:  _selectedIndex==2?Colors.white:Colors.blueGrey,),
+                title: Text("Chats",
+                  style: GoogleFonts.baloo2(
+                      color:  _selectedIndex==2?Colors.white:Colors.blueGrey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400
+                  ),)
             ),
             MoltenTab(
               icon: Icon(Icons.person,
                 color:  _selectedIndex==3?Colors.white:Colors.blueGrey,),
+                title: Text("Profile",
+                  style: GoogleFonts.baloo2(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400
+                  ),)
             ),
             MoltenTab(
               icon: Icon(Icons.headphones_rounded,
                 color:  _selectedIndex==4?Colors.white:Colors.blueGrey,),
+                title: Text("Help",
+                style: GoogleFonts.baloo2(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400
+                ),)
+
             ),
           ],
         ),
