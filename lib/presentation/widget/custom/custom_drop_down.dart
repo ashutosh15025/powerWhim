9521ce12a2d6173c1918d31ec3e8cfb6934f4647
ashutosh15025 <79096320+ashutosh15025@users.noreply.github.com>
2024-06-03 00:50:20 +0530

    chats initital

--- lib/presentation/widget/custom/custom_drop_down.dart
+++ lib/presentation/widget/custom/custom_drop_down.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:powerwhim/presentation/bloc/authbloc/auth_bloc.dart';
 import 'package:powerwhim/presentation/widget/custom/selected_drop_item_widget.dart';
 
 import '../../../constant/service_api_constant.dart';
 
 class CustomDropDown extends StatefulWidget {
-  const CustomDropDown({super.key, required this.dropDownHeading, required this.myList, required this.selectedItemFun});
+  const CustomDropDown(
+      {super.key, required this.dropDownHeading, required this.myList, required this.selectedItemFun, this.name});
+
   final String dropDownHeading;
   final List<String> myList;
+  final String ? name;
   final void Function(List<String>) selectedItemFun;
 
 
@@ -22,147 +27,187 @@ class _CustomDropDownState extends State<CustomDropDown> {
   List<String> selectedItem = [];
   bool notRelavent = false;
 
-  String ? selectedValue ; // Initial selection
+  String ? selectedValue; // Initial selection
   @override
   void initState() {
-    myList = myList +widget.myList;
+    myList = myList + widget.myList;
     print(widget.myList.length);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(widget.myList.length.toString()+"size");
-    List<DropdownMenuItem<String>> dropdownItems = myList.map((String value) => DropdownMenuItem<String>(
-      value: value,
-      child: Container(
-        child: Text(value,
-          style: TextStyle(
-              color: Colors.black
-          ),),
-      ),
-    )).toList();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("${widget.dropDownHeading}",
-              style: GoogleFonts.baloo2(
-                textStyle:TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
-                    color: Colors.white
-                ),)),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
-          width: double.infinity,
-          decoration: BoxDecoration(
-              border: Border.all(width: 1,color: Colors.yellow.shade500),
-              borderRadius: BorderRadius.circular(8)
+    print(widget.myList.length.toString() + "size ooooo");
+    List<DropdownMenuItem<String>> dropdownItems = myList.map((String value) =>
+        DropdownMenuItem<String>(
+          value: value,
+          child: Container(
+            child: Text(value,
+              style: TextStyle(
+                  color: Colors.black
+              ),),
           ),
-          child: DropdownButtonHideUnderline(
-            child: ButtonTheme(
-              alignedDropdown: true,
-              child: DropdownButton<String>(
-                hint: Text("${widget.dropDownHeading}",
-                    style: GoogleFonts.baloo2(
-                      textStyle:TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14,
-                          color: Colors.white
-                      ),)),
-                style: TextStyle(color: Colors.white),
-                selectedItemBuilder: (BuildContext context) {
-                  return myList.map<Widget>((String item) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(0,12,0,0),
-                      child: Text("Please select something",
-                          style: GoogleFonts.baloo2(
-                            textStyle:TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14,
-                                color: Colors.white
-                            ),)),
-                    );
-                  }).toList();
-                },
-                iconEnabledColor: Colors.yellow.shade500,
-                value: selectedValue,
-                items: dropdownItems,
-                onChanged: (String? newValue) {
-                  if(selectedItem.indexOf(newValue!)==-1)
-                  selectedItem.add(newValue!);
-                  setState(() {
-                    print(newValue.toString());
-                    widget.selectedItemFun(selectedItem);
-                    selectedValue = newValue!;
-                  });
-                },
+        )).toList();
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if(state is GetSportsSuccessState)
+        setState(() {
+          myList = state.sports;
+        });
+          print(myList.length);
+      },
+      builder: (context, state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("${widget.dropDownHeading}",
+                  style: GoogleFonts.baloo2(
+                    textStyle: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Colors.white
+                    ),)),
+            ),
+            InkWell(
+              onTap: (){
+                print("container");
+              },
+              child: Container(
+                margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.yellow.shade500),
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: InkWell(
+                  onTap: (){
+                  },
+                  child: DropdownButtonHideUnderline(
+                    child: InkWell(
+                      onTap: () {
+                        print("hi");
+                        if (true){
+                          BlocProvider.of<AuthBloc>(context).add(GetSportEvent());
+                         print("called");}
+                        else if (widget.name == "hobbies") {
+                          BlocProvider.of<AuthBloc>(context).add(GetHobbiesEvent());
+                        }
+                        else {
+
+                        }
+                      },
+                      child: ButtonTheme(
+                        alignedDropdown: true,
+                        child: InkWell(
+                          onTap: (){
+                            print("hii");
+                          },
+                          child: DropdownButton<String>(
+                            hint: Text("${widget.dropDownHeading}",
+                                style: GoogleFonts.baloo2(
+                                  textStyle: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                      color: Colors.white
+                                  ),)),
+                            style: TextStyle(color: Colors.white),
+                            selectedItemBuilder: (BuildContext context) {
+                              return myList.map<Widget>((String item) {
+                                return Padding(
+                                  padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+                                  child: Text("Please select something",
+                                      style: GoogleFonts.baloo2(
+                                        textStyle: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            color: Colors.white
+                                        ),)),
+                                );
+                              }).toList();
+                            },
+                            iconEnabledColor: Colors.yellow.shade500,
+                            value: selectedValue,
+                            items: dropdownItems,
+                            onChanged: (String? newValue) {
+                              if (selectedItem.indexOf(newValue!) == -1)
+                                selectedItem.add(newValue!);
+                              setState(() {
+                                print(newValue.toString());
+                                widget.selectedItemFun(selectedItem);
+                                selectedValue = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
-          child: Wrap(
-            spacing: 8.0, // Adjust spacing between chips
-            runSpacing: 4.0, // Adjust spacing between lines of chips
-            children: List<Widget>.generate(
-              selectedItem.length,
-                  (int index) {
-                return Chip(
-                  side: BorderSide(color: Color.fromRGBO(0, 156, 74, 1)),
-                  elevation: 0,
-                  shadowColor: Colors.white,
-                  deleteIconColor: Colors.white,
-                  backgroundColor: Color.fromRGBO(0, 156, 74, 1),
-                  shape: StadiumBorder(
-                  ),
-                  label: Text(selectedItem[index],
-                    style: GoogleFonts.baloo2(
-                        textStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500
-                        )
-                    ),),
-                  // You can also customize other properties of the Chip widget here
-                  onDeleted: () {
-                    setState(() {
-                      selectedItem.removeAt(index);
-                      widget.selectedItemFun(selectedItem);
-                    });
+            Padding(
+              padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: Wrap(
+                spacing: 8.0, // Adjust spacing between chips
+                runSpacing: 4.0, // Adjust spacing between lines of chips
+                children: List<Widget>.generate(
+                  selectedItem.length,
+                      (int index) {
+                    return Chip(
+                      side: BorderSide(color: Color.fromRGBO(0, 156, 74, 1)),
+                      elevation: 0,
+                      shadowColor: Colors.white,
+                      deleteIconColor: Colors.white,
+                      backgroundColor: Color.fromRGBO(0, 156, 74, 1),
+                      shape: StadiumBorder(
+                      ),
+                      label: Text(selectedItem[index],
+                        style: GoogleFonts.baloo2(
+                            textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500
+                            )
+                        ),),
+                      // You can also customize other properties of the Chip widget here
+                      onDeleted: () {
+                        setState(() {
+                          selectedItem.removeAt(index);
+                          widget.selectedItemFun(selectedItem);
+                        });
+                      },
+                    );
                   },
-                );
-              },
+                ),
+              ),
             ),
-          ),
-        ),
-        Row(
-          children: [
-            Checkbox(value: notRelavent, onChanged: (updateValue){
-              setState(() {
-                notRelavent = updateValue!;
-                selectedItem = [];
-                widget.selectedItemFun(selectedItem);
-              },
-              );
-            },
-            checkColor: Colors.white,
-                activeColor: Colors.green,),
-            Text("Not Relavant to me",
-            style: GoogleFonts.baloo2(
-              textStyle: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w500,
-                color: notRelavent?Colors.white:Colors.grey
-              )
-            ),)
+            Row(
+              children: [
+                Checkbox(value: notRelavent, onChanged: (updateValue) {
+                  setState(() {
+                    notRelavent = updateValue!;
+                    selectedItem = [];
+                    widget.selectedItemFun(selectedItem);
+                  },
+                  );
+                },
+                  checkColor: Colors.white,
+                  activeColor: Colors.green,),
+                Text("Not Relavant to me",
+                  style: GoogleFonts.baloo2(
+                      textStyle: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: notRelavent ? Colors.white : Colors.grey
+                      )
+                  ),)
+              ],
+            )
           ],
-        )
-      ],
+        );
+      },
     );
   }
 }
