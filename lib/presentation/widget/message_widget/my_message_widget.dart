7d7a0c4d025fecdb08c8bc5eee96e868a2744deb
Ashutosh615singh@gmail.com <ashutosh615singh@gmail.com>
2025-06-11 11:39:41 +0530

    image like whatsapp

--- lib/presentation/widget/message_widget/my_message_widget.dart
+++ lib/presentation/widget/message_widget/my_message_widget.dart
@@ -20,41 +20,44 @@ class MyMessageWidget extends StatelessWidget {
       backGroundColor: themeColor,
       child: Container(
         constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.7,
+          maxWidth: MediaQuery.of(context).size.width * 0.6,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-             image==null?SizedBox.shrink():Visibility(
-                visible: image!=null,
-                child: Container(
-                  color: Colors.grey,
-                  width: 400,
-                  height: 200,
-                  child: Image.network(
-                    "https://whim.ams3.digitaloceanspaces.com/"+
-                        image!,
-                    fit: BoxFit.fill,
-                    loadingBuilder: (BuildContext context, Widget child,
-                        ImageChunkEvent? loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return Container(
-                        height: 400,
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.yellowAccent,
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                                : null,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+            image == null
+            ? SizedBox.shrink()
+              : ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+      child:ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: 350,
+          minHeight: 200,// Limit max height
+          minWidth: double.infinity,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.network(
+            "https://whim.ams3.digitaloceanspaces.com/$image",
+            fit: BoxFit.cover, // Fill container, crop as needed
+            alignment: Alignment(0, -0.3), // Focus on center of the image
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.yellowAccent,
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                      loadingProgress.expectedTotalBytes!
+                      : null,
                 ),
-            ),
-            message==null?SizedBox.shrink():Text(
+              );
+            },
+          ),
+        ),
+      ),
+    ),
+        message==null?SizedBox.shrink():Text(
               message!,
               style: GoogleFonts.baloo2(
                 textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 16),
